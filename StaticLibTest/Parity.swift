88be2ba6a676f6bcb67404f0466bf8881b21fc0b
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2018-11-26 19:12:14 +0100

    updated wrapper to support returning data from libparity via Swift.Data type

--- StaticLibTest/Parity.swift
+++ StaticLibTest/Parity.swift
@@ -25,29 +25,38 @@ final class Parity {
     }
     
     func start() {
-        let test = ParityParams()
         let documentsDirectory = getDirectoryPath()
         print("Docs directory \(documentsDirectory)")
         guard let chainspecPath = Bundle.main.path(forResource: "custom_foundation", ofType: "json") else {
             return
         }
         print("Chain spec directory \(chainspecPath)")
-        let args = "parity --light --no-ipc --no-color --no-ws --no-jsonrpc --base-path=\(documentsDirectory) --chain=\(chainspecPath)"
+        let args = "parity --light --no-ipc --no-color --no-ws --no-jsonrpc --num-verifiers=2 --base-path=\(documentsDirectory) --chain=\(chainspecPath)"
+//        let args = "parity --light --no-ipc --no-color --no-ws --no-jsonrpc --num-verifiers=1 --base-path=\(documentsDirectory)"
         let step2 = parity_start_ios(&parityClientPointer, args)
-        print("parity_start returned \(step2)")
-        print("returned pointer: \(parityClientPointer)")
     }
 
     func testRPC() {
         print("Starting ``RPC test...")
         let testPayload = "{\"method\":\"net_peerCount\",\"params\":[],\"id\":1,\"jsonrpc\":\"2.0\"}"
+        var parityDataPointer: UnsafeMutablePointer<Int8>? = nil
+        var parityDataLength = 0
         if let client = parityClientPointer {
             var replyPointer: UnsafeMutablePointer<Int8>? = nil
-//            var mutableReplyPointer = UnsafeMutablePointer(mutating: replyPointer)
-            let result = parity_rpc_ios_query(client, testPayload, &replyPointer)
-            let reply = String(cString: replyPointer!)
-            parity_rpc_ios_release(replyPointer)
-            print("query result \(reply)Qbus kocha Asiunie}")
+            let result = parity_rpc_ios_query(client,
+                                              testPayload,
+                                              &parityDataPointer,
+                                              &parityDataLength)
+            if result == 0 {
+                if let dataPointer = parityDataPointer,
+                     parityDataLength > 0 {
+                    let data = Data(bytes: dataPointer, count: parityDataLength)
+                    if let string = String(data: data, encoding: .utf8) {
+                        print("\(string)")
+                    }
+                }
+                parity_rpc_ios_release(replyPointer)
+            }
         }
     }
 
