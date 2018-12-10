ae289229d7fa0411b2555c3c9d827554417e5139
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2018-12-10 19:47:10 +0100

    Refactor Parity wrapper to support new functionality

--- StaticLibTest/Parity.swift
+++ StaticLibTest/Parity.swift
@@ -10,11 +10,28 @@ import Foundation
 
 func onRestart(_: UnsafeMutableRawPointer?, _: UnsafePointer<Int8>?, _: Int) -> Void {}
 
+typealias LibraryCallback = (String) -> ()
+
 final class Parity {
 
     static let shared = Parity()
 
-    var parityClientPointer: UnsafeMutableRawPointer? = nil
+    var isParityRunning: Bool {
+        return parityClientPointer != nil
+    }
+
+    var parityLaunchArguments = "--light --no-ipc --no-color --no-ws --no-jsonrpc --num-verifiers=2"
+    var logCallback: LibraryCallback?
+    var responseCallback: LibraryCallback?
+
+    private var parityClientPointer: UnsafeMutableRawPointer? = nil
+    private var parityResponseDataPointer: UnsafeMutablePointer<Int8>? = nil
+    private var parityResponseDataLength: UInt = 0 {
+        didSet {
+            releaseQueryResponse()
+        }
+    }
+    private var parityResponseResultCode: Int32 = 0
 
     private init(){}
 
@@ -31,33 +48,64 @@ final class Parity {
             return
         }
         print("Chain spec directory \(chainspecPath)")
-        let args = "parity --light --no-ipc --no-color --no-ws --no-jsonrpc --num-verifiers=2 --base-path=\(documentsDirectory) --chain=\(chainspecPath)"
-//        let args = "parity --light --no-ipc --no-color --no-ws --no-jsonrpc --num-verifiers=1 --base-path=\(documentsDirectory)"
-        let step2 = parity_start_ios(&parityClientPointer, args)
+        let args = "parity \(parityLaunchArguments) --base-path=\(documentsDirectory) --chain=\(chainspecPath)"
+
+        let closure: LoggingCallback = { owner, pointer, length in
+            guard let owner = owner else { return }
+
+            let parity = Unmanaged<Parity>.fromOpaque(owner).takeUnretainedValue()
+            parity.loggingCallback(pointer, length)
+        }
+
+        let selfPointer = Unmanaged.passUnretained(self).toOpaque()
+        parity_logger_start(selfPointer, closure)
+        parity_set_panic_hook(selfPointer, closure)
+        let result = parity_start_ios(&parityClientPointer, args)
+        // check if result is 0
     }
 
-    func testRPC() {
-        print("Starting ``RPC test...")
-        let testPayload = "{\"method\":\"net_peerCount\",\"params\":[],\"id\":1,\"jsonrpc\":\"2.0\"}"
-        var parityDataPointer: UnsafeMutablePointer<Int8>? = nil
-        var parityDataLength = 0
-        if let client = parityClientPointer {
-            var replyPointer: UnsafeMutablePointer<Int8>? = nil
-            let result = parity_rpc_ios_query(client,
-                                              testPayload,
-                                              &parityDataPointer,
-                                              &parityDataLength)
-            if result == 0 {
-                if let dataPointer = parityDataPointer,
-                     parityDataLength > 0 {
-                    let data = Data(bytes: dataPointer, count: parityDataLength)
-                    if let string = String(data: data, encoding: .utf8) {
-                        print("\(string)")
-                    }
-                }
-                parity_rpc_ios_release(replyPointer)
-            }
+    func stop() {
+        guard let client = parityClientPointer else {
+            parityClientPointer = nil
+            return
         }
+        parity_destroy(client)
+        parityClientPointer = nil
+    }
+
+    func rpc(query: String) {
+        guard let client = parityClientPointer else {
+            return
+        }
+
+        parityResponseResultCode = parity_rpc_ios_query(client,
+                                          query,
+                                          &parityResponseDataPointer,
+                                          &parityResponseDataLength)
+    }
+
+    func loggingCallback(_ replyPointer: UnsafePointer<Int8>?, _ replyLength: UInt) {
+        guard let pointer = replyPointer else {
+            return
+        }
+        let data = Data(bytes: pointer, count: Int(replyLength))
+        if let string = String(data: data, encoding: .utf8) {
+            logCallback?(string)
+        }
+    }
+
+    private func releaseQueryResponse() {
+        guard let dataPointer = parityResponseDataPointer,
+            parityResponseDataLength > 0 else {
+                parityResponseDataPointer = nil
+                parityResponseDataLength = 0
+                return
+        }
+        let data = Data(bytes: dataPointer, count: Int(parityResponseDataLength))
+        if let string = String(data: data, encoding: .utf8) {
+            responseCallback?(string)
+        }
+        parity_rpc_ios_release(dataPointer)
     }
 
     private func getDirectoryPath() -> String {
