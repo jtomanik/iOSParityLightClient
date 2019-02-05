3a451fb45f87ab41a866dea444c580615f9d9ed9
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-02-05 15:31:08 +0100

    Adding test Mac app

---
+++ EthashMetalTest/GPUEthashTest/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  GPUEthashTest
+//
+//  Created by Jakub Tomanik on 03/02/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
+
+let gpu = EthashGPU()
+gpu?.testUnits()
+
