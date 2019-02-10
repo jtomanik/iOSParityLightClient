baf3589d64af77936102fa8b4cd93a956a73f406
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-02-10 10:20:43 +0100

    Refactored data_sizes header file to be used from all implementations

--- EthashMetalTest/libethash/data_sizes.h
+++ EthashMetalTest/libethash/data_sizes.h
@@ -20,15 +20,18 @@
  * @date 2015
  */
 
-#pragma once
+#ifndef data_sizes
+#define data_sizes
 
-#include <stdint.h>
+#if defined(__METAL_MACOS__) || defined(__METAL_IOS__)
+#define MEMSPACE constant
+#else
+#define MEMSPACE
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
-#include <stdint.h>
     
     // 2048 Epochs (~20 years) worth of tabulated DAG sizes
     
@@ -47,7 +50,7 @@ extern "C" {
     //             Sow[i*HashBytes]; j++]]]][[2]][[1]]
     
     
-    static const ethash_uint64_t dag_sizes[2048] = {
+    MEMSPACE static const ethash_uint64_t dag_sizes[2048] = {
         1073739904U, 1082130304U, 1090514816U, 1098906752U, 1107293056U,
         1115684224U, 1124070016U, 1132461952U, 1140849536U, 1149232768U,
         1157627776U, 1166013824U, 1174404736U, 1182786944U, 1191180416U,
@@ -476,7 +479,7 @@ extern "C" {
     //         While[! PrimeQ[i], i--];
     //         Sow[i*HashBytes]; j++]]]][[2]][[1]]
     
-    const ethash_uint64_t cache_sizes[2048] = {
+    MEMSPACE const ethash_uint64_t cache_sizes[2048] = {
         16776896U, 16907456U, 17039296U, 17170112U, 17301056U, 17432512U, 17563072U,
         17693888U, 17824192U, 17955904U, 18087488U, 18218176U, 18349504U, 18481088U,
         18611392U, 18742336U, 18874304U, 19004224U, 19135936U, 19267264U, 19398208U,
@@ -809,3 +812,5 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* data_sizes */
